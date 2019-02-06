47a8ccf33d62a2644668c7c8365498d383e8cffd
karlminor <karl_minor@hotmail.com>
2019-02-06 15:29:32 +0100

    Added init ai

---
+++ Search/src/ai/AI.java
@@ -0,0 +1,22 @@
+package ai;
+
+import game.gameboard.GameBoard;
+
+public class AI {
+	
+	public AI() {
+		
+	}
+	
+	public String miniMaxDecision(GameBoard gameBoard) {
+		return "";
+	}
+	
+	private int maxValue(GameBoard gameBoard) {
+		return 0;
+	}
+	
+	private int minValue(GameBoard gameBoard) {
+		return 0;
+	}
+}
